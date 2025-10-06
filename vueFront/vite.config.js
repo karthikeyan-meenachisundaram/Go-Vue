1bb6de813ad70643a99f8533e540a13b6122ba4c
Karthikeyan M <karthikeyan.meenachisundaram@wavicledata.com>
2025-10-06 20:29:31 +0530

    updated privateIP

--- vueFront/vite.config.js
+++ vueFront/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': 'http://172.31.45.255:8080' // Forward /api calls to backend
+      '/api': 'http://172.31.24.94:8080' // Forward /api calls to backend
       //'/api': 'http://localhost:8080' 
     }
   }
