5a66329237fdd2c09d438dceab5c4ed32f357a6e
karthikeyan-meenachisundaram <karthikeyan.meenachisundaram@wavicledata.com>
2025-10-06 19:51:56 +0530

    Update vite.config.js

--- vueFront/vite.config.js
+++ vueFront/vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      //'/api': 'http://172.31.45.255:8080' // Forward /api calls to backend
-      '/api': 'http://localhost:8080' 
+      '/api': 'http://172.31.45.255:8080' // Forward /api calls to backend
+      //'/api': 'http://localhost:8080' 
     }
   }
 })
